22ff4639e7a094a7481867a47bf01528089c2459
Thandolwethu Mpofu <mpofuthandolwethu@gmail.com>
2018-11-04 20:17:53 +0200

    Update: API index.ts

--- api/src/index.ts
+++ api/src/index.ts
@@ -5,13 +5,18 @@ import * as schema from './schema';
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
   ...schema,
-  context: req => ({
+  resolverValidationOptions: {
+    requireResolversForResolveType: false,
+  },
+  context: (req) => ({
     ...req,
     db: new Prisma({
       endpoint: 'https://eu1.prisma.sh/elandamor/pdbeats/dev', // the endpoint of the Prisma API
-      debug: true, // log all GraphQL queries & mutations sent to the Prisma API
+      debug: false, // log all GraphQL queries & mutations sent to the Prisma API
       // secret: 'mysecret123', // only needed if specified in `database/prisma.yml`
     }),
   }),
 })
-server.start(() => console.log('Server is running on http://localhost:4000'))
+
+// tslint:disable:no-console
+server.start(() => console.log('Server is running on http://localhost:4000'));
