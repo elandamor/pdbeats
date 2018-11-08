c6b4085498f3429714d1a37ecb79d361d6216911
Thandolwethu Mpofu <mpofuthandolwethu@gmail.com>
2018-11-08 12:27:34 +0200

    Feature: Introduce time formatting utilities

---
+++ api/src/utils/hh-mm-ss/index.ts
@@ -0,0 +1,30 @@
+// tslint:disable:no-magic-numbers
+
+/**
+ * Convert seconds(Int) to time(String)
+ * @param secs
+ */
+export const secondsToTime = (secs) => {
+  const hours = Math.floor(secs / 3600);
+  const minutes = Math.floor(secs % 3600 / 60);
+  const seconds = Math.floor(secs % 3600 % 60);
+
+	return (
+    hours === 0 ? '' : hours > 0
+    && hours.toString().length < 2 ? '0' + hours + ':' : hours + ':'
+  )
+  + ( minutes.toString().length < 2 ? '0' + minutes : minutes )
+  + ':'
+  + ( seconds.toString().length < 2 ? '0' + seconds : seconds );
+};
+
+/**
+ * Convert time(String) to seconds(Int)
+ * @param arg - time as a string
+ */
+export const timeToSeconds = (arg) => {
+  const time = arg.split(':');
+  const seconds = (parseInt(time[0], 10) * 60) + parseInt(time[1], 10);
+
+  return seconds;
+};
