1d27f30eef346defe2280a476314c2c0224e4b5a
Thandolwethu Mpofu <mpofuthandolwethu@gmail.com>
2018-11-16 15:37:32 +0200

    Update: hh-mm-ss secondsToTime method

--- app/lib/hh-mm-ss/index.ts
+++ app/lib/hh-mm-ss/index.ts
@@ -4,16 +4,16 @@
  * Convert seconds(Int) to time(String)
  * @param secs
  */
-export const secondsToTime = (secs: number) => {
+export const secondsToTime = (secs: number, showLeadingZero = false) => {
   const hours = Math.floor(secs / 3600);
   const minutes = Math.floor(secs % 3600 / 60);
   const seconds = Math.floor(secs % 3600 % 60);
 
 	return (
     hours === 0 ? '' : hours > 0
-    && hours.toString().length < 2 ? '0' + hours + ':' : hours + ':'
+    && hours.toString().length < 2 && showLeadingZero ? '0' + hours + ':' : hours + ':'
   )
-  + ( minutes.toString().length < 2 ? '0' + minutes : minutes )
+  + ( minutes.toString().length < 2 && showLeadingZero ? '0' + minutes : minutes )
   + ':'
   + ( seconds.toString().length < 2 ? '0' + seconds : seconds );
 };
