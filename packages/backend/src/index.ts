68cb23202ad45c2cfd0d7c1c295825b153cfde99
Thandolwethu Mpofu <mpofuthandolwethu@gmail.com>
2019-06-09 14:26:53 +0200

    refactor(*): Move form graphql-yoga to apollo-server.

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,23 +1,26 @@
-import { GraphQLServer, Options, PubSub } from 'graphql-yoga';
+import { ApolloServer, makeExecutableSchema } from 'apollo-server';
 import * as algoliasearch from 'algoliasearch';
+import { importSchema } from 'graphql-import';
+
 import { Prisma } from './generated/prisma';
-import * as schema from './schema';
+import * as defaultSchema from './schema';
 import { formatError } from './utils/apollo-errors';
 
 const algolia = algoliasearch('1P95FYQWIB', '0ec2d1a43dc56546e220a32c6465fc67');
 
-const options: Options = {
+const options: any = {
   formatError
 };
 
-const pubsub = new PubSub();
+const typeDefs = importSchema('./src/schema.graphql');
+const schema = makeExecutableSchema({
+  typeDefs,
+  ...defaultSchema,
+  resolverValidationOptions: { requireResolversForResolveType: false },
+});
 
-const server = new GraphQLServer({
-  typeDefs: './src/schema.graphql',
-  ...schema,
-  resolverValidationOptions: {
-    requireResolversForResolveType: false,
-  },
+const server = new ApolloServer({
+  schema,
   context: (req) => ({
     ...req,
     algolia,
@@ -26,10 +29,14 @@ const server = new GraphQLServer({
       debug: false, // log all GraphQL queries & mutations sent to the Prisma API
       // secret: 'mysecret123', // only needed if specified in `database/prisma.yml`
     }),
-    pubsub,
     userId: 'cjo6yn74ycxqx0a42rrsl3blv',
   }),
 })
 
 // tslint:disable:no-console
-server.start(options, () => console.log('Server is running on http://localhost:4000'));
+// server.listen(options, () => console.log('Server is running on http://localhost:4000'));
+// This `listen` method launches a web-server.  Existing apps
+// can utilize middleware options, which we'll discuss later.
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
